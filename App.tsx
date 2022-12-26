d067891ddf8f469eeb86a8ce6d3a5c58d3f74d3e
Antonio214 <antoniojuniorlk366@gmail.com>
2022-12-26 00:10:38 -0300

    added all popups to app

--- App.tsx
+++ App.tsx
@@ -1,26 +1,50 @@
 //App.tsx
 import { useState } from "react";
 import { Button, StyleSheet, View } from "react-native";
+import AttentionPopup from "./src/Components/AttentionPopup.tsx/AttentionPopup";
+import HelloWorldPopup from "./src/Components/HelloWorldPopup/HelloWorldPopup";
 import TwoButtonsLayoutPopup from "./src/Components/TwoButtonsLayoutPopup/TwoButtonsLayoutPopup";
 
 export default function App() {
-  const [shouldShowModal, setShouldShowModal] = useState(false);
+  const [shouldShowHelloModal, setShouldShowHelloModal] = useState(false);
+
+  const [shouldShowTwoBtnsModal, setShouldShowTwoBtnsModal] = useState(false);
+
+  const [shouldShowRedModal, setShouldShowRedModal] = useState(false);
 
   return (
     <View style={styles.container}>
       <Button
-        title={"Open Modal!"}
-        onPress={() => setShouldShowModal(true)}
+        title={"Open Hello Modal!"}
+        onPress={() => setShouldShowHelloModal(true)}
       ></Button>
 
+      <Button
+        title={"Open 2 Buttons Modal!"}
+        onPress={() => setShouldShowTwoBtnsModal(true)}
+      ></Button>
+
+      <Button
+        title={"Open Red Text Modal!"}
+        onPress={() => setShouldShowRedModal(true)}
+      ></Button>
+
+      <HelloWorldPopup
+        visible={shouldShowHelloModal}
+        closeCallback={() => setShouldShowHelloModal(false)}
+      />
+
       <TwoButtonsLayoutPopup
-        visible={shouldShowModal}
-        closeCallback={() => setShouldShowModal(false)}
-        confirmCallback={() => {
-          console.log("Confirm");
-          setShouldShowModal(false);
-        }}
-      ></TwoButtonsLayoutPopup>
+        confirmCallback={() => console.log("Confirmed by user!")}
+        visible={shouldShowTwoBtnsModal}
+        closeCallback={() => setShouldShowTwoBtnsModal(false)}
+      />
+
+      <AttentionPopup
+        message="Cuidado!"
+        visible={shouldShowRedModal}
+        closeCallback={() => setShouldShowRedModal(false)}
+      />
     </View>
   );
 }
@@ -30,6 +54,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
   },
 });
